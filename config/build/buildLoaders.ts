6f53a1c52b4cef4d9c528da5b3ab7662d5c79d87
Anna Pushkareva <smirnova_spb@mail.ru>
2024-02-09 13:00:25 +0300

    add cssLoaderWithModules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,11 +5,20 @@ import { BuildOptions } from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
 
+    const cssLoaderWithModules = {
+      loader: "css-loader",
+      options: {
+        modules: {
+          localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+        },
+      },
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          "css-loader",
+          cssLoaderWithModules,
           "sass-loader",
         ],
       }
