0b1eadd03928a6218715c96f7a20a86c130cc9c5
Anna Pushkareva <smirnova_spb@mail.ru>
2024-02-13 15:50:28 +0300

    change source-map

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -8,7 +8,6 @@ import { buildResolvers } from './buildResolvers';
 export function buildWebpack(options: BuildOptions): webpack.Configuration {
     const { mode, paths } = options;
     const isDev = mode === 'development';
-    const isProd = mode === 'production';
     
     return {
         mode: mode ?? 'development',
@@ -23,7 +22,7 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
             rules: buildLoaders(options),
           },
           resolve: buildResolvers(options),
-          devtool: isDev && 'inline-source-map',
+          devtool: isDev ? 'eval' : 'source-map',
           devServer: isDev ? buildDevServer(options) : undefined,
         }
 
